848c80f80c5a3bc334b68cf154a8b04c622ef8b8
MarParodi <parodi-2002@hotmail.com>
2026-08-25 10:04:15 -0700

    Fix-register email

--- src/main/java/com/example/ironplan/repository/OnboardingSessionRepository.java
+++ src/main/java/com/example/ironplan/repository/OnboardingSessionRepository.java
@@ -6,8 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface OnboardingSessionRepository extends JpaRepository<OnboardingSession, String> {
-    boolean existsByEmail(String email);
-    boolean existsByUsername(String username);
     Optional<OnboardingSession> findByToken(String token);
+    Optional<OnboardingSession> findFirstByEmailOrderByCreatedAtDesc(String email);
+    Optional<OnboardingSession> findFirstByUsernameOrderByCreatedAtDesc(String username);
 }
 
